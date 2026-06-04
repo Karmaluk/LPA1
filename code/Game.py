9f0a7333cbb470f42cf43d9b70d2f5ffffee31cb
Vinicius <karmaluk@gmail.com>
2026-06-03 20:42:46 -0400

    Added constants Added menu image Added game Title

--- code/Game.py
+++ code/Game.py
@@ -17,8 +17,3 @@ class Game:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()
-            #         quit()
